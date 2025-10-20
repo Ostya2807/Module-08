27297e24768ef0dad40e7f908be7cb8e863781d7
Ostya2807 <veres.sandor.daniel@gmail.com>
2025-10-20 20:59:11 +0200

    add some prefs to disable the password leak popup

--- Module-08/src/main/java/com/test/pages/BasePage.java
+++ Module-08/src/main/java/com/test/pages/BasePage.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class BasePage {
@@ -18,18 +20,19 @@ public class BasePage {
 
     public static WebDriver getDriver() {
         if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            Map<String,Object> prefs = new HashMap<>();
+            prefs.put("profile.password_manager_enabled", false);
+            prefs.put("profile.password_manager_leak_detection", false);
+            prefs.put("profile.credentials_enable_service", false);
+
+            options.setExperimentalOption("prefs", prefs);
+
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
         return driver;
-    }
-
-    public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
     }
 
     protected void waitForVisibility(WebElement element) {
